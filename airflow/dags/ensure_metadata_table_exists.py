8018de7b4349a1116a8f097cb561ac9f94270805
matttriano <matt.triano@gmail.com>
2022-11-12 11:09:22 -0600

    Implements sqlalchemy db connection and schema creation utils and applies in a DAG.

--- airflow/dags/ensure_metadata_table_exists.py
+++ airflow/dags/ensure_metadata_table_exists.py
@@ -1,11 +1,13 @@
 import datetime as dt
-import os
+from typing import Dict
 
 from airflow.decorators import dag, task
-from sqlalchemy.engine.url import URL
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from sqlalchemy.engine.base import Engine
 
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from utils.db import database_has_schema, execute_structural_command
+
 
 def check_that_schema_exists(schema_name: str, conn_id: str = "dwh_db_conn") -> bool:
     pg_hook = PostgresHook(conn_id)
@@ -17,6 +19,16 @@ def check_that_schema_exists(schema_name: str, conn_id: str = "dwh_db_conn") -> 
     )
     return len(rows) > 0
 
+
+def get_pg_engine(conn_id: str) -> Dict:
+    try:
+        pg_hook = PostgresHook(conn_id)
+        engine = pg_hook.get_sqlalchemy_engine()
+        return {"engine": engine}
+    except Exception as e:
+        print(f"Failed to generate engine to pg db using conn_id {conn_id}. Error: {e}, {type(e)}")
+
+
 @dag(
     schedule=None,
     start_date=dt.datetime(2022, 11, 1),
@@ -24,42 +36,43 @@ def check_that_schema_exists(schema_name: str, conn_id: str = "dwh_db_conn") -> 
     tags=["metadata"],
 )
 def ensure_metadata_table_exists():
-    
     @task
-    def check_for_metadata_schema(conn_id: str) -> bool:
-        return check_that_schema_exists(conn_id=conn_id, schema_name="metadata")
-
-    @task
-    def create_metadata_schema(conn_id: str = "dwh_db_conn") -> bool:
-        pg_hook = PostgresHook(conn_id)
-        rows = pg_hook.get_records(
-            """SELECT * FROM metadata.table_metadata;"""
-        )
-        return len(rows) > 0
+    def get_dwh_db_engine(conn_id: str = "dwh_db_conn") -> Dict:
+        return get_pg_engine(conn_id=conn_id)
 
     @task
-    def check_for_metadata_table(conn_id: str = "dwh_db_conn") -> bool:
-        pg_hook = PostgresHook(conn_id)
-        rows = pg_hook.get_records(
-            """SELECT * FROM metadata.table_metadata;"""
-        )
+    def db_has_metadata_schema(engine: Engine) -> bool:
+        return database_has_schema(engine=engine, schema_name="metadata")
 
-        return len(rows) > 0
+    @task
+    def create_metadata_schema(engine: Engine) -> None:
+        schema_name = "metadata"
+        try:
+            username = engine.url.username
+            execute_structural_command(
+                query=f"""
+                    GRANT USAGE ON SCHEMA {schema_name} TO {username};
+                    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA {schema_name} TO {username};
+                    ALTER DEFAULT PRIVILEGES FOR USER {username} IN SCHEMA {schema_name}
+                        GRANT ALL ON TABLES TO {username};
+                """,
+                engine=engine,
+            )
+        except Exception as e:
+            print(f"Failed to create metadata schema. Error: {e}, {type(e)}")
 
-    
-    # create_pet_table = PostgresOperator(
-    #     task_id="check_for",
-    #     sql="""
-    #         CREATE TABLE IF NOT EXISTS pet (
-    #         pet_id SERIAL PRIMARY KEY,
-    #         name VARCHAR NOT NULL,
-    #         pet_type VARCHAR NOT NULL,
-    #         birth_date DATE NOT NULL,
-    #         OWNER VARCHAR NOT NULL);
-    #       """,
-    # )
+    # @task
+    # def check_for_metadata_table(conn_id: str = "dwh_db_conn") -> bool:
+    #     pg_hook = PostgresHook(conn_id)
+    #     rows = pg_hook.get_records(
+    #         """SELECT * FROM metadata.table_metadata;"""
+    #     )
+    #     return len(rows) > 0
 
-    metadata_schema_exists = check_for_metadata_schema()
-    create_metadata_schema()
+    engine_dict = get_dwh_db_engine(conn_id="dwh_db_conn")
+    metadata_schema_exists = db_has_metadata_schema(engine=engine_dict["engine"])
+    if not metadata_schema_exists:
+        create_metadata_schema(engine=engine_dict["engine"])
+
 
 ensure_metadata_table_exists()
---
+++ airflow/dags/utils/db.py
@@ -0,0 +1,54 @@
+from typing import List
+
+import pandas as pd
+from sqlalchemy import inspect, text
+from sqlalchemy.schema import CreateSchema
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.dialects.postgresql import insert
+
+
+def get_data_schema_names(engine: Engine) -> List:
+    insp = inspect(engine)
+    return insp.get_schema_names()
+
+
+def database_has_schema(engine: Engine, schema_name: str) -> bool:
+    with engine.connect() as conn:
+        return engine.dialect.has_schema(connection=conn, schema=schema_name)
+
+
+def get_data_table_names_in_schema(engine: Engine, schema_name: str) -> List:
+    insp = inspect(engine)
+    return insp.get_table_names(schema=schema_name)
+
+
+def create_database_schema(engine: Engine, schema_name: str, verbose: bool = False) -> None:
+    if not database_has_schema(engine=engine, schema_name=schema_name):
+        with engine.connect() as conn:
+            conn.execute(CreateSchema(name=schema_name))
+            conn.commit()
+            if verbose:
+                print(f"Database schema '{schema_name}' successfully created.")
+    else:
+        if verbose:
+            print(f"Database schema '{schema_name}' already exists.")
+
+
+def execute_result_returning_query(query: str, engine: Engine) -> pd.DataFrame:
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        results_df = pd.DataFrame(result.fetchall(), columns=result.keys())
+        if engine._is_future:
+            conn.commit()
+    return results_df
+
+
+def execute_structural_command(query: str, engine: Engine) -> None:
+    with engine.connect() as conn:
+        with conn.begin():
+            conn.execute(text(query))
+
+
+def to_sql_on_conflict_do_nothing(table, conn, keys, data_iter) -> None:
+    data = [dict(zip(keys, row)) for row in data_iter]
+    conn.execute(insert(table.table).on_conflict_do_nothing(), data)
