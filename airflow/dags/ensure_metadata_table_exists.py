5297737aee46f51da5d76a0119e678f5ee126555
matttriano <matt.triano@gmail.com>
2022-11-15 19:55:50 -0600

    Sets up the intended logical flow of the create metadata schema DAG.

--- airflow/dags/ensure_metadata_table_exists.py
+++ airflow/dags/ensure_metadata_table_exists.py
@@ -3,14 +3,16 @@ import logging
 from typing import Dict
 
 from airflow.decorators import dag, task
-from airflow.models.baseoperator import chain
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import BranchPythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.utils.trigger_rule import TriggerRule
+
+# from airflow.providers.postgres.operators.postgres import PostgresOperator
 from sqlalchemy.engine.base import Engine
 
-from utils.db import database_has_schema, execute_structural_command
+from utils.db import (
+    database_has_schema,
+    execute_structural_command,
+    get_data_table_names_in_schema,
+)
 
 
 task_logger = logging.getLogger("airflow.task")
@@ -35,19 +37,25 @@ def ensure_metadata_table_exists():
                 f"Failed to generate engine to pg db using conn_id {conn_id}. Error: {e}, {type(e)}"
             )
 
-    def db_has_metadata_schema(conn_id: str) -> bool:
+    @task.branch
+    def db_has_metadata_schema(conn_id: str) -> str:
         if not database_has_schema(engine=_get_pg_engine(conn_id=conn_id), schema_name="metadata"):
+            task_logger.info(
+                f"Schema 'metadata' not in database. Branching to create_metadata_schema task."
+            )
             return "create_metadata_schema"
         else:
-            return "end"
+            task_logger.info(
+                f"Schema 'metadata' in database. Branching to table_exists_in_schema task."
+            )
+            return "table_exists_in_schema"
 
     @task
     def end() -> str:
-        return "end"
+        task_logger.info(f"Ending DAG execution.")
 
     @task
     def create_metadata_schema() -> None:
-        schema_name = "metadata"
         task_logger.info(f"inside create_metadata_schema")
         engine = _get_pg_engine(conn_id=POSTGRES_CONN_ID)
         try:
@@ -65,16 +73,34 @@ def ensure_metadata_table_exists():
         except Exception as e:
             print(f"Failed to create metadata schema. Error: {e}, {type(e)}")
 
-    metadata_schema_exists_branch = BranchPythonOperator(
-        task_id="check_for_metadata_schema_1",
-        python_callable=db_has_metadata_schema,
-        op_args=(POSTGRES_CONN_ID,),
-    )
+    @task.branch
+    def table_exists_in_schema(
+        conn_id: str, schema_name: str = "metadata", table_name: str = "metadata"
+    ) -> str:
+        tables_in_metadata_schema = get_data_table_names_in_schema(
+            engine=_get_pg_engine(conn_id=conn_id), schema_name=schema_name
+        )
+        if table_name not in tables_in_metadata_schema:
+            task_logger.info(f"Table {table_name} not found in schema {schema_name}. Creating")
+            return "create_metadata_table"
+        else:
+            task_logger.info(f"Table {table_name} found in schema {schema_name}.")
+            return "end"
+
+    @task
+    def create_metadata_table(conn_id: str) -> str:
+        task_logger.info(f"Placeholder for create_metadata_table functionality.")
+
+    metadata_schema_exists_branch_1 = db_has_metadata_schema(conn_id=POSTGRES_CONN_ID)
 
     create_metadata_schema_1 = create_metadata_schema()
+    table_exists_in_schema_1 = table_exists_in_schema(conn_id=POSTGRES_CONN_ID)
+    create_metadata_table_1 = create_metadata_table(conn_id=POSTGRES_CONN_ID)
     end_1 = end()
 
-    metadata_schema_exists_branch >> [create_metadata_schema_1, end_1]
+    metadata_schema_exists_branch_1 >> create_metadata_schema_1 >> create_metadata_table_1 >> end_1
+    metadata_schema_exists_branch_1 >> table_exists_in_schema_1 >> create_metadata_table_1 >> end_1
+    metadata_schema_exists_branch_1 >> table_exists_in_schema_1 >> end_1
 
 
 chack_metadata_schema_dag = ensure_metadata_table_exists()
