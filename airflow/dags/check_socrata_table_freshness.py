e9800199e8a4e4b52a15b3f023063e81f50b38d9
matttriano <matt.triano@gmail.com>
2022-11-17 23:21:25 -0600

    Resolves issue in setting the table freshness metadata attribute, still have to debug the third task.

--- airflow/dags/check_socrata_table_freshness.py
+++ airflow/dags/check_socrata_table_freshness.py
@@ -8,7 +8,7 @@ from airflow.operators.empty import EmptyOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.trigger_rule import TriggerRule
-from sqlalchemy.engine.base import Engine
+import pandas as pd
 
 from utils.db import get_pg_engine
 from utils.socrata import SocrataTable, SocrataTableMetadata
@@ -37,37 +37,44 @@ def check_table_freshness():
         return socrata_metadata
 
     @task
-    def get_table_freshness_check_record(
+    def extract_table_freshness_info(
         socrata_metadata: SocrataTableMetadata, conn_id: str
-    ) -> Tuple:
+    ) -> pd.DataFrame:
         engine = get_pg_engine(conn_id=conn_id)
-        insertable_record = socrata_metadata.get_insertable_check_table_metadata_record(
-            engine=engine
+        socrata_metadata.check_table_metadata(engine=engine)
+        task_logger.info(
+            f"Extracted table freshness information.",
+            f"Fresh source data available: {socrata_metadata.table_check_metadata['updated_data_available']}",
+            f"Fresh source metadata available: {socrata_metadata.table_check_metadata['updated_metadata_available']}",
+        )
+        return socrata_metadata
+
+    @task
+    def ingest_table_freshness_check_metadata(socrata_metadata: SocrataTableMetadata, conn_id: str):
+        engine = get_pg_engine(conn_id=conn_id)
+        insertable_record_df = socrata_metadata.get_insertable_check_table_metadata_record(
+            engine=engine, output_type="DataFrame"
+        )
+        task_logger.info(f"type(insertable_record_df): {type(insertable_record_df)}")
+        insertable_record_df.to_sql(
+            name="table_metadata",
+            schema="metadata",
+            con=engine,
+            if_exists="append",
         )
         task_logger.info(
-            f"Produced insertable record of freshness check results. Record: {insertable_record}"
+            f"Ingested table freshness check results into metadata table. Record: {insertable_record_df}"
         )
-        return insertable_record
 
     socrata_table_metadata_1 = get_socrata_table_metadata(socrata_table=chicago_cta_stations_table)
-    table_freshness_check_record_1 = get_table_freshness_check_record(
+    socrata_table_metadata_2 = extract_table_freshness_info(
         socrata_metadata=socrata_table_metadata_1, conn_id=POSTGRES_CONN_ID
     )
-    ingest_table_freshness_check_metadata_1 = PostgresOperator(
-        task_id="ingest_table_freshness_check_metadata",
-        postgres_conn_id=POSTGRES_CONN_ID,
-        sql=f"""
-            INSERT INTO metadata.table_metadata
-            VALUES {table_freshness_check_record_1};
-        """,
-        trigger_rule=TriggerRule.ALL_DONE,
+    ingest_freshness_metadata_1 = ingest_table_freshness_check_metadata(
+        socrata_metadata=socrata_table_metadata_2, conn_id=POSTGRES_CONN_ID
     )
 
-    (
-        socrata_table_metadata_1
-        >> table_freshness_check_record_1
-        >> ingest_table_freshness_check_metadata_1
-    )
+    (socrata_table_metadata_1 >> socrata_table_metadata_2 >> ingest_freshness_metadata_1)
 
 
 check_table_freshness_dag = check_table_freshness()
