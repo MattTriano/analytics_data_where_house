e8eeb1aa13d437294133ad0d465a973f74b382a9
matttriano <matt.triano@gmail.com>
2022-12-10 09:27:07 -0600

    Refactors data_raw schema creating DAG and add DAG stub for running cleaning and transformation steps for Cook County Parcel Sales.

---
+++ airflow/dags/cook_county/clean_cook_county_parcel_sales.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import logging
+
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from airflow.utils.trigger_rule import TriggerRule
+
+from cc_utils.socrata import SocrataTable
+
+SOCRATA_TABLE = SocrataTable(table_id="wvhk-k5uv", table_name="cook_county_parcel_sales")
+
+
+@dag(
+    schedule="0 6 4 * *",
+    start_date=dt.datetime(2022, 11, 1),
+    catchup=False,
+    tags=["cook_county", "parcels", "fact_table", "data_raw"],
+)
+def clean_cook_county_parcel_sales():
+    POSTGRES_CONN_ID = "dwh_db_conn"
+
+    update_data_raw_table_1 = BashOperator(
+        task_id="update_data_raw_table",
+        bash_command=f"""cd /opt/airflow/dbt && \
+            dbt run --select models/intermediate/{SOCRATA_TABLE.table_name}_clean.sql""",
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
+    )
+    update_data_raw_table_1
+
+
+clean_cook_county_parcel_sales()
