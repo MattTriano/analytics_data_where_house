18fc2715cf45cb68b1d4b9f5ac147426d79ae6dc
matttriano <matt.triano@gmail.com>
2023-01-26 08:42:30 -0600

    Reviewed and edited CTA train station and cc nbhd boundary stubs.

--- airflow/dbt/models/intermediate/chicago_cta_train_stations_standardized.sql
+++ airflow/dbt/models/intermediate/chicago_cta_train_stations_standardized.sql
@@ -1,34 +1,27 @@
 {{ config(materialized='view') }}
-{% set ck_cols = [
-        REPLACE_WITH_COMPOSITE_KEY_COLUMNS
-] %}
+{% set ck_cols = ["stop_id"] %}
 
 WITH records_with_basic_cleaning AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(ck_cols) }} AS REPLACE_WITH_BETTER_id,
-        upper(location_state::text) AS location_state,
-        upper(location_zip::text) AS location_zip,
-        upper(station_descriptive_name::text) AS station_descriptive_name,
-        upper(blue::text) AS blue,
-        upper(station_name::text) AS station_name,
-        upper(y::text) AS y,
-        upper(location_address::text) AS location_address,
-        upper(location_city::text) AS location_city,
-        upper(brn::text) AS brn,
-        upper(direction_id::text) AS direction_id,
-        upper(map_id::text) AS map_id,
-        upper(g::text) AS g,
-        upper(stop_name::text) AS stop_name,
-        upper(p::text) AS p,
-        upper(ada::text) AS ada,
-        upper(pnk::text) AS pnk,
-        upper(pexp::text) AS pexp,
-        upper(stop_id::text) AS stop_id,
-        upper(red::text) AS red,
-        upper(o::text) AS o,
-        geometry::GEOMETRY(POINT,4326) AS geometry,
-        source_data_updated::timestamptz AS source_data_updated,
-        ingestion_check_time::timestamptz AS ingestion_check_time
+        upper(stop_id::text)                  AS stop_id,
+        upper(station_name::text)             AS station_name,
+        upper(station_descriptive_name::text) AS station_descriptive_name,        
+        upper(direction_id::char(1))          AS direction_id,
+        map_id::int                           AS map_id,
+        upper(stop_name::text)                AS stop_name,
+        red::boolean                          AS red_line,
+        o::boolean                            AS orange_line,
+        pnk::boolean                          AS pink_line,
+        y::boolean                            AS yellow_line,
+        g::boolean                            AS green_line,
+        blue::boolean                         AS blue_line,
+        p::boolean                            AS purple_line,
+        pexp::boolean                         AS purple_express,
+        brn::boolean                          AS brown_line,
+        ada::boolean                          AS ada,
+        geometry::GEOMETRY(POINT,4326)        AS geometry,
+        source_data_updated::timestamptz      AS source_data_updated,
+        ingestion_check_time::timestamptz     AS ingestion_check_time
     FROM {{ ref('chicago_cta_train_stations') }}
     ORDER BY {% for ck in ck_cols %}{{ ck }}{{ "," if not loop.last }}{% endfor %}
 )
