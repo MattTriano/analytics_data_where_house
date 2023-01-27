fde53ace5e7518f2ee920c6bb50666449ca3e163
matttriano <matt.triano@gmail.com>
2023-01-26 18:26:42 -0600

    Implements branching task to avoid recreating a dbt _standardized stage model if a finished model exists.

--- airflow/dags/dev_experiments/rollout_standardized_dbt_model_dev.py
+++ airflow/dags/dev_experiments/rollout_standardized_dbt_model_dev.py
@@ -3,9 +3,11 @@ import datetime as dt
 from logging import Logger
 import logging
 from pathlib import Path
+import os
 from typing import List
 
 from airflow.decorators import dag, task
+from airflow.models.baseoperator import chain
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.trigger_rule import TriggerRule
@@ -120,6 +122,44 @@ def make_dbt_standardized_model(table_name: str, conn_id: str, task_logger: Logg
     task_logger.info(f"Leaving make_dbt_standardized_model")
 
 
+@task.branch(trigger_rule=TriggerRule.NONE_FAILED)
+def dbt_standardized_model_ready(table_name: str, task_logger: Logger, **kwargs) -> str:
+    airflow_home = os.environ["AIRFLOW_HOME"]
+    task_logger.info(f"airflow_home: {airflow_home}")
+    task_logger.info(f"table_name:   {table_name}")
+    file_path = Path(airflow_home).joinpath(
+        "dbt", "models", "intermediate", f"{table_name}_standardized.sql"
+    )
+    host_file_path = str(file_path).replace(airflow_home, "/airflow")
+    task_logger.info(f"file_path:         {file_path}")
+    task_logger.info(f"host_file_path:    {host_file_path}")
+    if file_path.is_file():
+        with open(file_path, "r") as f:
+            file_lines = f.readlines()
+        for file_line in file_lines:
+            if (
+                "REPLACE_WITH_COMPOSITE_KEY_COLUMNS" in file_line
+                or "REPLACE_WITH_BETTER_id" in file_line
+            ):
+                raise Exception(
+                    f"Found unfinished stub for dbt _standardized model in "
+                    + f"{host_file_path}. Please finish and try again"
+                )
+        task_logger.info(f"Found a _standardized stage dbt model that looks finished; Proceeding")
+        return "endpoint"
+    else:
+        task_logger.info(f"No _standardized stage dbt model found.")
+        task_logger.info(f"Creating a stub in loc: {host_file_path}")
+        task_logger.info(f"Edit the stub before proceeding to generate _clean stage dbt models.")
+        return "make_dbt_standardized_model"
+
+
+@task
+def endpoint(task_logger: Logger) -> None:
+    task_logger.info("Ending run")
+    return "end"
+
+
 @dag(
     schedule=SOCRATA_TABLE.schedule,
     start_date=dt.datetime(2022, 11, 1),
@@ -128,12 +168,17 @@ def make_dbt_standardized_model(table_name: str, conn_id: str, task_logger: Logg
 )
 def dev_rollout_standardized_dbt_model():
 
+    std_model_exists_1 = dbt_standardized_model_ready(
+        table_name=SOCRATA_TABLE.table_name,
+        task_logger=task_logger,
+    )
     make_standardized_stage_1 = make_dbt_standardized_model(
         table_name=SOCRATA_TABLE.table_name,
         conn_id="dwh_db_conn",
         task_logger=task_logger,
     )
-    make_standardized_stage_1
+    endpoint_1 = endpoint(task_logger=task_logger)
+    chain(std_model_exists_1, [make_standardized_stage_1, endpoint_1])
 
 
 dev_rollout_standardized_dbt_model()
