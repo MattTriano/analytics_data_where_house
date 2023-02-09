31638a46f8878bdb912f6c28ec3231569d84a4be
matttriano <matt.triano@gmail.com>
2023-02-08 23:25:13 -0600

    Engineers current owner feature and added a file to materialize related features to a dwh table.

--- airflow/dbt/models/feature/cook_county_parcel_location_features.sql
+++ airflow/dbt/models/feature/cook_county_parcel_location_features.sql
@@ -4,6 +4,7 @@ WITH parcel_loc_features AS (
     SELECT
         parcel_location_id,
         pin,
+        CONCAT(township, nbhd) AS town_nbhd,
         CASE
             WHEN property_address = mailing_address THEN true
             WHEN 
@@ -11,31 +12,33 @@ WITH parcel_loc_features AS (
                 AND property_address IS NOT NULL
                 AND mailing_address IS NOT NULL THEN false
             ELSE NULL
-        END CASE AS owner_occupied,
-        property_address,
-        property_apt_no,
-        property_city,
-        property_zip,
-        nbhd,
-        township,
-        township_name,
-        CONCAT(township, nbhd),
-        municipality,
-        municipality_fips,
-        ward,
-        tract_geoid,
-        ohare_noise,
-        floodplain,
-        fs_flood_factor,
-        fs_flood_risk_direction,
-        withinmr100,
-        withinmr101300,
-        longitude,
-        latitude,
-        geometry
+        END CASE AS owner_occupied
     FROM {{ ref('cook_county_parcel_locations_clean') }}
+),
+parcel_buyers AS (
+    SELECT 
+        pin, 
+        sale_buyer_name,
+        row_number() over(
+			   PARTITION BY pin ORDER BY pin, sale_date DESC
+		   ) as rn
+    FROM {{ ref('cook_county_parcel_sales_clean') }}
+),
+latest_buyer AS (
+    SELECT 
+        pin,
+        sale_buyer_name AS owner_name
+    FROM latest_buyer
+    WHERE rn = 1
 )
 
-SELECT *
-FROM parcel_loc_features
-ORDER BY pin, sale_date
+SELECT
+    lf.parcel_location_id,
+    lf.pin,
+    lf.town_nbhd,
+    lf.owner_occupied,
+    lb.owner_name
+FROM parcel_loc_features AS lf
+INNER JOIN latest_buyer AS lb
+ON lf.pin = lb.pin
+ORDER BY lf.pin,
