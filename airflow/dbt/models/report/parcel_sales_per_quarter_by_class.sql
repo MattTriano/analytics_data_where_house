ab9ec2d4428e9a78110da41e839aff1bd2c76ead
matttriano <matt.triano@gmail.com>
2023-02-15 19:41:51 -0600

    Adds reports of sales per quarter by class and class+town_nbhd.

--- airflow/dbt/models/report/parcel_sales_per_quarter_by_class.sql
+++ airflow/dbt/models/report/parcel_sales_per_quarter_by_class.sql
@@ -1,32 +1,32 @@
 {{ config(materialized='table') }}
-{% set min_count_in_period = 3 %}
+{% set min_count_in_period = 2 %}
 
-WITH sale_counts_per_group_per_month AS (
+WITH sale_counts_per_group_per_qtr AS (
 	SELECT
 		count(*),
-		to_char(sale_date, 'YYYY-MM')       AS month_of_sale
+		to_char(sale_date, 'YYYY-Q') AS qtr_of_sale
 	FROM {{ ref('cook_county_parcel_sales_fact') }}
-	GROUP BY month_of_sale
+	GROUP BY qtr_of_sale
 ),
 end_dates AS (
-	SELECT to_char(month_of_sale, 'YYYY-MM') AS month_of_sale
+	SELECT to_char(qtr_of_sale, 'YYYY-Q') AS qtr_of_sale
 	FROM generate_series(
 		(
-			SELECT (min(month_of_sale) || '-01')::date
-			FROM sale_counts_per_group_per_month
+			SELECT (date_part('year', min(to_date(qtr_of_sale, 'YYYY-Q'))) || '-01-01')::date
+			FROM sale_counts_per_group_per_qtr
 			WHERE count >= {{ min_count_in_period }}
 		),
 		(
-			SELECT (max(month_of_sale) || '-01')::date
-			FROM sale_counts_per_group_per_month
+			SELECT (date_part('year', max(to_date(qtr_of_sale, 'YYYY-Q'))) || '-01-01')::date
+			FROM sale_counts_per_group_per_qtr
 			WHERE count >= {{ min_count_in_period }}
 		),
-		interval '1 month'
-	) AS month_of_sale
+		interval '3 months'
+	) AS qtr_of_sale
 ),
 sales_stats_per_group AS (
 	SELECT
-		to_char(sale_date, 'YYYY-MM')       AS month_of_sale,
+		to_char(sale_date, 'YYYY-Q')       AS qtr_of_sale,
 		class,
 		class_descr,
 		count(sale_price)                   AS num_sales,
@@ -35,12 +35,12 @@ sales_stats_per_group AS (
 		min(sale_price)                     AS min_sale_price,
 		percentile_cont(0.5) WITHIN GROUP (order by sale_price) AS median_sale_price	
 	FROM {{ ref('cook_county_parcel_sales_fact') }}
-	GROUP BY class_descr, class, month_of_sale
-	ORDER BY month_of_sale, class
+	GROUP BY class_descr, class, qtr_of_sale
+	ORDER BY qtr_of_sale, class
 )
 
 SELECT
-	ed.month_of_sale,
+	ed.qtr_of_sale,
 	gss.class,
 	COALESCE(gss.class_descr, 'DEPRECATED CLASS') AS class_descr,
 	COALESCE(gss.num_sales, 0)                    AS num_sales,
@@ -50,5 +50,5 @@ SELECT
 	COALESCE(gss.median_sale_price, 0)            AS median_sale_price
 FROM end_dates AS ed
 LEFT JOIN sales_stats_per_group AS gss
-ON ed.month_of_sale = gss.month_of_sale
-ORDER BY ed.month_of_sale, gss.class
+ON ed.qtr_of_sale = gss.qtr_of_sale
+ORDER BY ed.qtr_of_sale, gss.class
