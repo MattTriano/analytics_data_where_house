31aa4168c1edb7b4edde843be169afe641af0673
matttriano <matt.triano@gmail.com>
2023-04-06 23:06:27 -0500

    Generated clean model for Chicago vacant buildings data set.

--- airflow/dags/make_and_run_dbt_models_for_a_data_set.py
+++ airflow/dags/make_and_run_dbt_models_for_a_data_set.py
@@ -19,7 +19,7 @@ from cc_utils.file_factory import (
 )
 from tasks.socrata_tasks import highlight_unfinished_dbt_standardized_stub
 
-from sources.tables import NYC_PARCEL_SALES as SOCRATA_TABLE
+from sources.tables import CHICAGO_VACANT_AND_ABANDONED_BUILDINGS as SOCRATA_TABLE
 
 task_logger = logging.getLogger("airflow.task")
 
---
+++ airflow/dbt/models/clean/chicago_vacant_and_abandoned_buildings_clean.sql
@@ -0,0 +1,50 @@
+{{ config(materialized='view') }}
+{% set ck_cols = ["violation_number, "property_address", "entity_or_person_s_"] %}
+{% set record_id = "vacant_bldg_violation_id" %}
+{% set base_cols = [
+    "vacant_bldg_violation_id", "violation_number", "property_address", "entity_or_persons",
+    "issued_date", "violation_type", "docket_number", "last_hearing_date", "issuing_department",
+    "disposition_description", "total_fines", "interest_amount", "total_administrative_costs",
+    "original_total_amount_due", "collection_costs_or_attorney_fees", "court_cost", "total_paid",
+    "current_amount_due", "latitude", "longitude", "geometry", "source_data_updated",
+    "ingestion_check_time"
+] %}
+
+-- selects all records from the standardized view of this data
+WITH std_data AS (
+    SELECT *
+    FROM {{ ref('chicago_vacant_and_abandoned_buildings_standardized') }}
+),
+
+-- keeps the most recently updated version of each record 
+std_records_numbered_latest_first AS (
+    SELECT *,
+        row_number() over(partition by {{record_id}} ORDER BY source_data_updated DESC) as rn
+    FROM std_data
+),
+most_current_records AS (
+    SELECT *
+    FROM std_records_numbered_latest_first
+    WHERE rn = 1
+),
+
+-- selects the source_data_updated (ie the date of publication) value from each record's
+--   first ingestion into the local data warehouse
+std_records_numbered_earliest_first AS (
+    SELECT *,
+        row_number() over(partition by {{record_id}} ORDER BY source_data_updated ASC) as rn
+FROM std_data
+),
+records_first_ingested_pub_date AS (
+    SELECT {{record_id}}, source_data_updated AS first_ingested_pub_date
+    FROM std_records_numbered_earliest_first
+    WHERE rn = 1
+)
+
+SELECT
+    {% for bc in base_cols %}mcr.{{ bc }},{% endfor %}
+    fi.first_ingested_pub_date
+FROM most_current_records AS mcr
+LEFT JOIN records_first_ingested_pub_date AS fi
+ON mcr.{{ record_id }} = fi.{{ record_id }}
+ORDER BY {% for ck in ck_cols %}mcr.{{ ck }} DESC, {% endfor %} mcr.source_data_updated DESC
