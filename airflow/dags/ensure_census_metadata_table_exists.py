90dc27c075efae438cd3005c363f5758ca8b6304
matttriano <matt.triano@gmail.com>
2023-04-10 18:55:13 -0500

    Starts implementing a CensusTableMetadata class with update logic.

--- airflow/dags/ensure_census_metadata_table_exists.py
+++ airflow/dags/ensure_census_metadata_table_exists.py
@@ -9,12 +9,6 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.edgemodifier import Label
 from airflow.utils.trigger_rule import TriggerRule
 
-from cc_utils.db import (
-    get_pg_engine,
-    database_has_schema,
-    execute_structural_command,
-    get_data_table_names_in_schema,
-)
 
 from tasks.metadata_tasks import (
     metadata_schema_exists,
@@ -26,85 +20,6 @@ from tasks.metadata_tasks import (
 task_logger = logging.getLogger("airflow.task")
 
 POSTGRES_CONN_ID = "dwh_db_conn"
-
-
-# @task.branch(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
-# def metadata_schema_exists(conn_id: str) -> str:
-#     if not database_has_schema(engine=get_pg_engine(conn_id=conn_id), schema_name="metadata"):
-#         task_logger.info(
-#             f"Schema 'metadata' not in database. Branching to create_metadata_schema task."
-#         )
-#         return "create_metadata_schema"
-#     else:
-#         task_logger.info(
-#             f"Schema 'metadata' in database. Branching to table_exists_in_schema task."
-#         )
-#         return "metadata_table_exists"
-
-
-# @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
-# def create_metadata_schema() -> None:
-#     task_logger.info(f"inside create_metadata_schema")
-#     engine = get_pg_engine(conn_id=POSTGRES_CONN_ID)
-#     try:
-#         username = engine.url.username
-#         execute_structural_command(
-#             query=f"""
-#             CREATE SCHEMA metadata;
-#             GRANT USAGE ON SCHEMA metadata TO {username};
-#             GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA metadata TO {username};
-#             ALTER DEFAULT PRIVILEGES FOR USER {username} IN SCHEMA metadata
-#                 GRANT ALL ON TABLES TO {username};
-#             """,
-#             engine=engine,
-#         )
-#     except Exception as e:
-#         print(f"Failed to create metadata schema. Error: {e}, {type(e)}")
-
-
-# @task.branch(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
-# def census_metadata_table_exists(
-#     table_name: str, conn_id: str, task_logger: Logger
-# ) -> str:
-#     schema_name = "metadata"
-#     table_name = "census_metadata"
-#     tables_in_metadata_schema = get_data_table_names_in_schema(
-#         engine=get_pg_engine(conn_id=conn_id), schema_name=schema_name
-#     )
-#     if table_name not in tables_in_metadata_schema:
-#         task_logger.info(f"Table {table_name} not found in schema {schema_name}. Creating")
-#         task_logger.info(f"tables_in_metadata_schema: {tables_in_metadata_schema}")
-#         return "create_census_metadata_table"
-#     else:
-#         task_logger.info(f"Table {table_name} found in schema {schema_name}.")
-#         return "end"
-
-
-# @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
-# def create_census_metadata_table(conn_id: str, task_logger: Logger):
-#     try:
-#         table_name = "census_metadata"
-#         task_logger.info(f"Creating table metadata.{table_name}")
-#         postgres_hook = PostgresHook(postgres_conn_id=conn_id)
-#         conn = postgres_hook.get_conn()
-#         cur = conn.cursor()
-#         cur.execute(
-#             f"""CREATE TABLE IF NOT EXISTS metadata.{table_name} (
-#                     id SERIAL PRIMARY KEY,
-#                     metadata_url TEXT NOT NULL,
-#                     last_modified TIMESTAMP NOT NULL,
-#                     size TEXT,
-#                     description TEXT,
-#                     is_dir BOOLEAN,
-#                     is_file BOOLEAN,
-#                     time_of_check TIMESTAMP WITH TIME ZONE NOT NULL,
-#                     updated_data_available BOOLEAN DEFAULT NULL
-#                 );"""
-#         )
-#         conn.commit()
-#         return "success"
-#     except Exception as e:
-#         print(f"Failed to create metadata table {table_name}. Error: {e}, {type(e)}")
 
 
 @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
@@ -124,7 +39,7 @@ def create_metadata_table(conn_id: str, task_logger: Logger):
                     is_dir BOOLEAN,
                     is_file BOOLEAN,
                     time_of_check TIMESTAMP WITH TIME ZONE NOT NULL,
-                    updated_data_available BOOLEAN DEFAULT NULL
+                    updated_metadata_available BOOLEAN DEFAULT NULL
                 );"""
         )
         conn.commit()
@@ -141,12 +56,15 @@ def create_metadata_table(conn_id: str, task_logger: Logger):
 )
 def a_dev_create_census_metadata_table():
 
-    metadata_schema_exists_branch_1 = metadata_schema_exists(conn_id=POSTGRES_CONN_ID, task_logger=task_logger)
-    create_metadata_schema_1 = create_metadata_schema(conn_id=POSTGRES_CONN_ID, task_logger=task_logger)
+    metadata_schema_exists_branch_1 = metadata_schema_exists(
+        conn_id=POSTGRES_CONN_ID, task_logger=task_logger
+    )
+    create_metadata_schema_1 = create_metadata_schema(
+        conn_id=POSTGRES_CONN_ID, task_logger=task_logger
+    )
     metadata_table_exists_1 = metadata_table_exists(
         table_name="census_metadata", conn_id=POSTGRES_CONN_ID, task_logger=task_logger
     )
-    # table_exists_in_schema_1 = census_metadata_table_exists(conn_id=POSTGRES_CONN_ID)
     create_metadata_table_1 = create_metadata_table(
         conn_id=POSTGRES_CONN_ID, task_logger=task_logger
     )
