4b2db9ab4e4b1bf0a76d05d6177bfd9adc23318d
matttriano <matt.triano@gmail.com>
2023-05-26 06:12:36 -0500

    Refactors a refresh_census_api_vars_metadata DAG task into a function to streamline the DAG, and handles the case where no dataset groups data exists.

--- airflow/dags/refresh_census_api_dataset_variables_metadata.py
+++ airflow/dags/refresh_census_api_dataset_variables_metadata.py
@@ -11,7 +11,6 @@ from sqlalchemy.dialects.postgresql import insert as pg_insert
 
 from cc_utils.census import CensusAPIHandler, CensusDatasetSource
 from tasks.census_tasks import (
-    check_api_dataset_freshness,
     get_census_api_data_handler,
     get_latest_catalog_freshness_from_db,
 )
@@ -27,8 +26,20 @@ task_logger = logging.getLogger("airflow.task")
 pd.options.display.max_columns = None
 
 POSTGRES_CONN_ID = "dwh_db_conn"
-DATASET_IDENTIFIER = "https://api.census.gov/data/id/ECNSIZE2017"
+DATASET_IDENTIFIER = "https://api.census.gov/data/id/PDBTRACT2022"
 MAX_DAYS_BEFORE_REFRESH = 30
+
+
+def get_days_since_last_api_catalog_freshness_check(
+    freshness_df: pd.DataFrame, task_logger: Logger
+) -> pd.DataFrame:
+    time_of_last_check = freshness_df["time_of_check"].max()
+    time_since_last_check = (
+        pd.Timestamp.now(tz=time_of_last_check.tz) - freshness_df["time_of_check"].max()
+    )
+    task_logger.info(f"Time of most recent Census API Data Catalog check: {time_of_last_check}")
+    task_logger.info(f"Time since the last check: {time_since_last_check}")
+    return time_since_last_check.days
 
 
 @task
@@ -80,22 +91,12 @@ def get_latest_dateset_variables_from_db(
 
 
 @task
-def get_days_since_last_api_catalog_freshness_check(
-    freshness_df: pd.DataFrame, task_logger: Logger
-) -> pd.DataFrame:
-    time_of_last_check = freshness_df["time_of_check"].max()
-    time_since_last_check = (
-        pd.Timestamp.now(tz=time_of_last_check.tz) - freshness_df["time_of_check"].max()
-    )
-    task_logger.info(f"Time of most recent Census API Data Catalog check: {time_of_last_check}")
-    task_logger.info(f"Time since the last check: {time_since_last_check}")
-    return time_since_last_check.days
-
-
-@task
 def get_fresh_enough_api_handler(
-    days_since_refresh: int, max_days_before_refresh: int, task_logger: Logger, **kwargs
+    local_metadata_df: pd.DataFrame, max_days_before_refresh: int, task_logger: Logger, **kwargs
 ) -> CensusAPIHandler:
+    days_since_refresh = get_days_since_last_api_catalog_freshness_check(
+        freshness_df=local_metadata_df, task_logger=task_logger
+    )
     task_logger.info(f"days_since_refresh: {days_since_refresh}")
     task_logger.info(f"max_days_before_refresh: {max_days_before_refresh}")
     if days_since_refresh > max_days_before_refresh:
@@ -123,6 +124,7 @@ def update_api_dataset_variables_metadata(
     identifier: str, api_handler: CensusAPIHandler, conn_id: str, task_logger: Logger
 ) -> CensusDatasetSource:
     variables_df = api_handler.prepare_dataset_variables_metadata_df(identifier=identifier)
+    task_logger.info(f"Dataset variables in the searched Census dataset: {len(variables_df)}")
     engine = get_pg_engine(conn_id=conn_id)
     api_dataset_variables_metadata_table = get_reflected_db_table(
         engine=engine,
@@ -165,9 +167,9 @@ def update_api_dataset_geographies_metadata(
 @task
 def update_api_dataset_groups_metadata(
     identifier: str, api_handler: CensusAPIHandler, conn_id: str, task_logger: Logger
-) -> CensusDatasetSource:
+) -> str:
     groups_df = api_handler.prepare_dataset_groups_metadata_df(identifier=identifier)
-    if groups_df is not None:
+    if (groups_df is not None) and (len(groups_df) > 0):
         engine = get_pg_engine(conn_id=conn_id)
         api_dataset_groups_metadata_table = get_reflected_db_table(
             engine=engine,
@@ -188,17 +190,21 @@ def update_api_dataset_groups_metadata(
             f"New dataset groups added to group metadata table: "
             + f"{len(ingested_api_dataset_groups_df)}"
         )
-        return ingested_api_dataset_groups_df
+        return "Successfully_ingested_groups_metadata"
+    else:
+        return "No_groups_metadata_to_ingest"
 
 
 @task.branch(trigger_rule=TriggerRule.NONE_FAILED_OR_SKIPPED)
 def fresher_dataset_variables_metadata_available(
-    days_since_refresh: int,
     local_metadata_df: pd.DataFrame,
     local_variables_metadata_df: pd.DataFrame,
     max_days_before_refresh: int,
     task_logger: Logger,
 ) -> str:
+    days_since_refresh = get_days_since_last_api_catalog_freshness_check(
+        freshness_df=local_metadata_df, task_logger=task_logger
+    )
     if days_since_refresh > max_days_before_refresh:
         task_logger.info(
             f"Census API Dataset Metadata was last pulled over {days_since_refresh} days ago,\n"
@@ -239,9 +245,6 @@ def refresh_census_api_dataset_variables_metadata():
     local_freshness_1 = get_latest_catalog_freshness_from_db(
         conn_id=POSTGRES_CONN_ID, task_logger=task_logger
     )
-    days_since_last_check = get_days_since_last_api_catalog_freshness_check(
-        freshness_df=local_freshness_1, task_logger=task_logger
-    )
     local_dataset_freshness_1 = get_latest_dataset_freshness_from_db(
         freshness_df=local_freshness_1,
         identifier=DATASET_IDENTIFIER,
@@ -251,14 +254,13 @@ def refresh_census_api_dataset_variables_metadata():
         identifier=DATASET_IDENTIFIER, conn_id=POSTGRES_CONN_ID, task_logger=task_logger
     )
     fresher_variables_metadata_exists_1 = fresher_dataset_variables_metadata_available(
-        days_since_refresh=days_since_last_check,
         local_metadata_df=local_dataset_freshness_1,
         local_variables_metadata_df=single_dataset_check_1,
         max_days_before_refresh=MAX_DAYS_BEFORE_REFRESH,
         task_logger=task_logger,
     )
     api_handler = get_fresh_enough_api_handler(
-        days_since_refresh=days_since_last_check,
+        local_metadata_df=local_dataset_freshness_1,
         max_days_before_refresh=MAX_DAYS_BEFORE_REFRESH,
         task_logger=task_logger,
     )
