dabcb04476d377a977198ff05dbe979cb94f9a4c
matttriano <matt.triano@gmail.com>
2023-06-13 06:29:21 -0500

    Adds tests for valid groups and tags responses.

--- airflow/tests/dags/cc_utils/census/test_core.py
+++ airflow/tests/dags/cc_utils/census/test_core.py
@@ -37,9 +37,41 @@ def test_get_dataset_groups_metadata_when_no_groups(mocker):
     )
 
 
+def test_get_dataset_groups_metadata_with_a_valid_group(mocker):
+    mocker.patch("cc_utils.census.core.get_url_response", side_effect=lambda url: read_file(url))
+    groups_df = get_dataset_groups_metadata(
+        groups_url=Path(".")
+        .resolve()
+        .joinpath("tests/dags/cc_utils/census/data/valid_groups_resp.json")
+    )
+    assert groups_df.equals(
+        pd.DataFrame(
+            {
+                "group_name": ["AB2000CSCBO"],
+                "group_description": [
+                    "Annual Business Survey: Owner Characteristics of Respondent Employer Firms by Sector, Sex, Ethnicity, Race, and Veteran Status for the U.S., States, and Metro Areas: 2020"
+                ],
+                "group_variables": [
+                    "http://api.census.gov/data/2020/abscbo/groups/AB2000CSCBO.json"
+                ],
+            }
+        )
+    )
+
+
 def test_get_dataset_tags_metadata_when_null_tags_url(mocker):
     mocker.patch(
         "cc_utils.census.core.get_url_response", side_effect=lambda url: mock_get_url_response(url)
     )
     tags_df = get_dataset_tags_metadata(tags_url=None)
     assert tags_df.equals(pd.DataFrame({"tag_name": ["no_tags"]}))
+
+
+def test_get_dataset_tags_metadata_with_valid_tags(mocker):
+    mocker.patch("cc_utils.census.core.get_url_response", side_effect=lambda url: read_file(url))
+    tags_df = get_dataset_tags_metadata(
+        tags_url=Path(".")
+        .resolve()
+        .joinpath("tests/dags/cc_utils/census/data/valid_tags_resp.json")
+    )
+    assert tags_df.equals(pd.DataFrame({"tag_name": ["ethnicity", "race", "sex", "veteran"]}))
