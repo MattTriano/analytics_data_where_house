be0038f069bbeacbd55573caff341f7656a4e533
matttriano <matt.triano@gmail.com>
2023-06-19 10:56:41 -0500

    Adds a pipeline for roadways (which are a county aggregated TIGER distribution).

---
+++ airflow/dags/geography/update_cook_county_roads_2022.py
@@ -0,0 +1,45 @@
+import datetime as dt
+import logging
+
+from airflow.decorators import dag
+from airflow.models.baseoperator import chain
+
+from tasks.tiger_tasks import (
+    fresher_source_data_available,
+    request_and_ingest_fresh_data,
+    local_data_is_fresh,
+    record_data_update,
+    check_freshness,
+)
+
+from sources.tiger_datasets import COOK_COUNTY_ROADS_2022 as TIGER_DATASET
+
+task_logger = logging.getLogger("airflow.task")
+POSTGRES_CONN_ID = "dwh_db_conn"
+
+
+@dag(
+    schedule=TIGER_DATASET.schedule,
+    start_date=dt.datetime(2022, 11, 1),
+    catchup=False,
+    tags=["illinois", "census", "geospatial"],
+)
+def update_cook_county_roads_2022():
+    freshness_check_1 = check_freshness(
+        tiger_dataset=TIGER_DATASET, conn_id=POSTGRES_CONN_ID, task_logger=task_logger
+    )
+    update_available_1 = fresher_source_data_available(
+        freshness_check=freshness_check_1, task_logger=task_logger
+    )
+    local_data_is_fresh_1 = local_data_is_fresh(task_logger=task_logger)
+    update_data_1 = request_and_ingest_fresh_data(
+        tiger_dataset=TIGER_DATASET, conn_id=POSTGRES_CONN_ID, task_logger=task_logger
+    )
+
+    record_update_1 = record_data_update(conn_id=POSTGRES_CONN_ID, task_logger=task_logger)
+
+    chain(freshness_check_1, update_available_1, [update_data_1, local_data_is_fresh_1])
+    chain(update_data_1, record_update_1)
+
+
+update_cook_county_roads_2022()
--- airflow/dags/sources/tiger_datasets.py
+++ airflow/dags/sources/tiger_datasets.py
@@ -1,5 +1,17 @@
 from cc_utils.census.tiger import TIGERDataset
-from sources.geographies import ILLINOIS_CENSUS_TRACTS, ENTIRE_UNITED_STATES
+from sources.geographies import (
+    COOK_COUNTY_CENSUS_TRACTS,
+    ENTIRE_UNITED_STATES,
+    ILLINOIS_CENSUS_TRACTS,
+)
+
+COOK_COUNTY_ROADS_2022 = TIGERDataset(
+    base_dataset_name="cook_county_roads",
+    vintage_year=2022,
+    entity_name="ROADS",
+    geography=COOK_COUNTY_CENSUS_TRACTS,
+    schedule="25 3 2 * *",
+)
 
 ILLINOIS_CENSUS_TRACTS_2022 = TIGERDataset(
     base_dataset_name="illinois_census_tracts",
