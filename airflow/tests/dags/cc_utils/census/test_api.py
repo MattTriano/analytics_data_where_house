f4a3ced008bc5dbfe0d2236dbccf77362bafda5d
matttriano <matt.triano@gmail.com>
2023-06-19 18:45:02 -0500

    Adds geog class for Counties, as well as some tests.

--- airflow/tests/dags/cc_utils/census/test_api.py
+++ airflow/tests/dags/cc_utils/census/test_api.py
@@ -1,4 +1,6 @@
-from cc_utils.census.api import CensusGeogTract, CensusGeogBlockGroup
+import pytest
+
+from cc_utils.census.api import CensusGeogTract, CensusGeogBlockGroup, CensusGeogCounty
 
 
 def test_create_CensusGeogBlockGroup_multiple_counties():
@@ -25,3 +27,23 @@ def test_create_CensusGeogTract_multiple_counties():
 def test_create_CensusGeogTract_single_county():
     tract_obj = CensusGeogTract(state_cd="17", county_cd="031")
     assert tract_obj.api_call_geographies == "for=tract:*&in=state:17&in=county:031"
+
+
+def test_create_CensusGeogCounty_multi_state_all_counties():
+    county_obj = CensusGeogCounty(state_cd=["17", "18"], county_cd="*")
+    assert county_obj.api_call_geographies == "for=county:*&in=state:17,18"
+
+
+def test_create_CensusGeogCounty_multi_state_specific_counties_raises_ValueError():
+    with pytest.raises(ValueError):
+        _ = CensusGeogCounty(state_cd=["17", "18"], county_cd="031")
+
+
+def test_create_CensusGeogCounty_single_state_all_counties():
+    county_obj = CensusGeogCounty(state_cd="17", county_cd="*")
+    assert county_obj.api_call_geographies == "for=county:*&in=state:17"
+
+
+def test_create_CensusGeogCounty_single_state_one_county():
+    county_obj = CensusGeogCounty(state_cd="17", county_cd="031")
+    assert county_obj.api_call_geographies == "for=county:031&in=state:17"
