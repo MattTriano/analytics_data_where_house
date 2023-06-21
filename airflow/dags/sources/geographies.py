1602562af4de7bec52e7c60c8f103e56c7c4c8f8
matttriano <matt.triano@gmail.com>
2023-06-20 23:07:13 -0500

    Adds dbt models produced by running other TIGER geom DAGs with new flow.

--- airflow/dags/sources/geographies.py
+++ airflow/dags/sources/geographies.py
@@ -12,5 +12,6 @@ MIDWEST_CENSUS_TRACTS = CensusGeogTract(
     state_cd=["17", "18", "19", "20", "26", "27", "29", "31", "37", "39", "46", "55"]
 )
 ILLINOIS_CENSUS_TRACTS = CensusGeogTract(state_cd="17")
+ILLINOIS_COUNTIES = CensusGeogCounty(state_cd="17", county_cd="*")
 ENTIRE_UNITED_STATES = CensusGeogUS()
 UNITED_STATES_COUNTIES = CensusGeogCounty(state_cd="*", county_cd="*")
